0aa255cfd27c883df07ed64d59673db27495062a
Ryan  Vitor <127523720+RyanVitorr@users.noreply.github.com>
2026-01-16 11:56:39 -0300

    novo deploy

--- myfilmelist/src/App.tsx
+++ myfilmelist/src/App.tsx
@@ -1,22 +1,19 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import MinhaLista from './pages/lista/MinhaLista';
-import Detalhes from './pages/detalhes/Detalhes';
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import MinhaLista from './pages/lista/MinhaLista'
+import Detalhes from './pages/detalhes/Detalhes'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/Login" element={<Login/>}/>
-        <Route path="/Minha-lista" element={<MinhaLista/>}/>
-        <Route path="/detalhes/:id" element={<Detalhes/>}/>
-      </Routes>
-    </BrowserRouter>
-  );
-};
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/minha-lista" element={<MinhaLista />} />
+      <Route path="/detalhes/:id" element={<Detalhes />} />
+    </Routes>
+  )
+}
 
-export default App;
+export default App
